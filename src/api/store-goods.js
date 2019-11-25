42f463b05faf17908ff3e5f5e3baed1dd0adf38e
airzhanglin <1015448230@qq.com>
2019-11-25 11:54:40 +0800

    up

--- src/api/store-goods.js
+++ src/api/store-goods.js
@@ -28,3 +28,14 @@ export function setUpdatePriceStock(data) {
     noMerCode: true
   })
 }
+
+// 根据按照门店-规格维度更新门店规格状态
+
+export function setUpdateStoreData(data) {
+  return request({
+    url: `${config.merGoods}/1.0/store-spec/store-spec`,
+    method: 'put',
+    data: data,
+    noMerCode: true
+  })
+}
