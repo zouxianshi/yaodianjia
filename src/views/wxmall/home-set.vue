025bc77ee7af7f9ded7de1124fe51950151f148a
hezhixing <314290737@qq.com>
2019-12-10 15:38:55 +0800

    update code

--- src/views/wxmall/home-set.vue
+++ src/views/wxmall/home-set.vue
@@ -834,7 +834,7 @@ export default {
       return this.$store.state.user.merCode || ''
     },
     upLoadUrl() {
-      return `${this.uploadFileURL}/${config.merGoods}/1.0/file/_upload?merCode=${this.merCode}`
+      return `${this.uploadFileURL}${config.merGoods}/1.0/file/_upload?merCode=${this.merCode}`
     },
     headers() {
       return { 'Authorization': this.$store.getters.token }
