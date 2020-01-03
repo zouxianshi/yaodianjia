aae55d982ef0929e50b068371e8b7cb1e47dfa59
张乐 <zhangle@hydee.com>
2020-01-03 17:37:41 +0800

    perf: 在线咨询消息框限制最大输入长度

--- src/views/customerService/consultation.js
+++ src/views/customerService/consultation.js
@@ -246,6 +246,13 @@ export default {
         })
         return
       }
+      if (this.textMsgValue.length >= 512) {
+        this.$message({
+          type: 'warning',
+          message: '消息长度不能超过512个字！'
+        })
+        return
+      }
       console.log('sendMsg')
       var msgInfo = {
         content: this.textMsgValue,
