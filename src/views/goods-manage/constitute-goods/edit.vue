111e136aa515cadc24cfb4988f0dd842ccd79bdc
e <425947944@qq.com>
2020-01-19 18:29:37 +0800

    订单bug

--- src/views/goods-manage/constitute-goods/edit.vue
+++ src/views/goods-manage/constitute-goods/edit.vue
@@ -329,9 +329,9 @@ export default {
       },
       // childCommodities: [], // 子商品信息
       basicRules: {
-        // name: [{ required: true, message: '请输入商品名称', trigger: 'blur' },
-        //   { min: 1, max: 30, message: '长度在 1 到 30 个字', trigger: 'blur' }
-        // ],
+        name: [{ required: true, message: '请输入商品名称', trigger: 'blur' },
+          { min: 1, max: 30, message: '长度在 1 到 30 个字', trigger: 'blur' }
+        ],
         image: [{ required: true, message: '请上传图片', trigger: 'blur' }],
         keyWord: [{ min: 1, max: 30, message: '长度在 1 到 30 个字', trigger: 'blur' }],
         number: [{ required: true, trigger: 'blur' }],
