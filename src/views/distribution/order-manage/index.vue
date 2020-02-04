eef094cef4698542f12816368fa097dce0d3d514
gesineng@hydee.cn <gesineng@hydee.cn>
2020-02-04 23:58:25 +0800

    style=

--- src/views/distribution/order-manage/index.vue
+++ src/views/distribution/order-manage/index.vue
@@ -1,13 +1,177 @@
 <template>
-  <div />
+  <div class="container">
+    <div class="header-panel">
+      <div class="box">
+        <div class="item" :class="'active'" @click="setDialogContent('')">
+          全部
+        </div>
+        <div class="item">待到货</div>
+        <div class="item">待核销</div>
+        <div class="item">已完成</div>
+      </div>
+      <el-select
+        v-model="value"
+        filterable
+        placeholder="选择门店"
+        class="search"
+      >
+        <el-option
+          v-for="item in options"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value"
+        />
+      </el-select>
+      <el-input
+        v-model="input2"
+        placeholder="请输入内容"
+        prefix-icon="el-icon-search"
+        class="search"
+      />
+    </div>
+    <div class="table-panel">
+      <table-card />
+    </div>
+    <div class="pagination-panel">
+      <el-pagination
+        :current-page="currentPage4"
+        :page-sizes="[10, 20, 50, 100]"
+        :page-size="10"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="4"
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+      />
+    </div>
+    <el-dialog
+      :title="dialogTitle"
+      :visible.sync="dialogVisible"
+      append-to-body
+    >
+      <el-table :data="dialogContent">
+        <el-table-column property="pic" label="商品图片" width="150">
+          <div class="dialog-pic">暂无图片</div></el-table-column>
+        <el-table-column
+          property="name"
+          label="商品名称/编号"
+          width="200"
+        />
+        <el-table-column property="price" label="单价" />
+        <el-table-column property="count" label="数量" />
+      </el-table>
+      <div style="display:flex;flex-direction:row;margin-top:40px;">
+        <div style="color:#000;font-size:14px">预约单号：</div>
+        <div style="color:#000;font-size:14px;margin-left:20px;">
+          123456789012345
+        </div>
+      </div>
+      <div style="display:flex;flex-direction:row;margin-top:40px;">
+        <div style="color:#000;font-size:14px">预约单号：</div>
+        <div style="color:#000;font-size:14px;margin-left:20px;">
+          123456789012345
+        </div>
+      </div>
+      <div style="display:flex;flex-direction:row;margin-top:40px;">
+        <div style="color:#000;font-size:14px">预约单号：</div>
+        <div style="color:#000;font-size:14px;margin-left:20px;">
+          123456789012345
+        </div>
+      </div>
+      <div
+        style="display:flex;flex-direction:row;margin-top:40px;justify-content: center;"
+      >
+        <el-button>取消</el-button>
+        <el-button type="primary">确认</el-button>
+      </div>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
+import tableCard from './table-card/index.vue'
 export default {
-
+  components: { tableCard },
+  data() {
+    return {
+      dialogVisible: false,
+      dialogTitle: '',
+      dialogContent: []
+    }
+  },
+  methods: {
+    setDialogContent(type) {
+      this.dialogVisible = true
+      if (type === 'writeoff') {
+        this.dialogTitle = '确认核销'
+      } else {
+        this.dialogTitle = '确认到货 (签收后预约单进入核销状态，请提醒用户上门)'
+      }
+    }
+  }
 }
 </script>
 
-<style lang="sass" scoped>
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
 
+  .header-panel {
+    padding: 10px 25px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    .search {
+      width: 284px;
+      margin-left: 44px;
+    }
+    .box {
+      border-radius: 6px;
+      display: flex;
+      flex-direction: row;
+      border: 1px solid #d7d7d7;
+      flex-shrink: 0;
+      .item {
+        padding: 12px;
+        font-size: 14px;
+        background: #fff;
+        color: #4a4a4a;
+        cursor: pointer;
+        display: inline-block;
+
+        &.active {
+          color: #fff;
+          background: #157de8;
+        }
+      }
+    }
+  }
+  .table-panel {
+    padding: 0 25px;
+  }
+  .dialog-pic {
+    width: 72px;
+    height: 72px;
+    text-align: center;
+    line-height: 72px;
+    font-size: 14px;
+    color: #9b9b9b;
+    background-color: #ebebeb;
+  }
+  #content-panel {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    .default {
+      color: #000000;
+      font-size: 14px;
+    }
+    .txt {
+      color: #000000;
+      font-size: 14px;
+      margin-left: 20px;
+    }
+  }
+}
 </style>
---
+++ src/views/distribution/order-manage/table-card/index.vue
@@ -0,0 +1,169 @@
+<template>
+  <div>
+    <div class="table-header">
+      <!-- <el-checkbox class="check-box" /> -->
+      <div class="table-column flex-1">商品</div>
+      <div class="table-column flex-2">商品编号</div>
+      <div class="table-column flex-1">单价</div>
+      <div class="table-column flex-1">数量</div>
+      <div class="table-column flex-1">预约人</div>
+      <div class="table-column flex-1">预约时间</div>
+      <div class="table-column flex-1">订单状态</div>
+      <div class="table-column flex-2">提货门店</div>
+    </div>
+    <div class="table-content">
+      <div class="header">
+        <div class="gray">
+          预约单编号：12452904985048
+        </div>
+        <div class="blue">订单详情</div>
+      </div>
+      <div class="row">
+        <div class="table-column content-container flex-3">
+          <div class="image-box">
+            <img src="" class="pic">
+            <div class="box">暂无图片</div>
+          </div>
+          <div class="content-box">
+            <div class="desc">
+              预约单编号：12452904985048预约单编号：12452904985048预约单编号：12452904985048预约单编号：12452904985048
+            </div>
+            <div class="order">
+              预约单编号：12452904985048
+              预约单编号：12452904985048预约单编号：12452904985048预约单编
+            </div>
+          </div>
+        </div>
+        <div class="table-column content-center flex-1">单价</div>
+        <div class="table-column content-center flex-1">数量</div>
+        <div class="table-column content-center flex-1">预约人</div>
+        <div class="table-column content-center flex-1">预约时间</div>
+        <div class="table-column content-center flex-1">订单状态</div>
+        <div class="table-column content-center flex-2">
+          长沙市岳麓区麓山南路店 NO.1234长沙市岳麓区麓山南路店
+          NO.1234长沙市岳麓区麓山南路店 NO.1234
+        </div>
+      </div>
+    </div>
+    <div class="table-empty" />
+  </div>
+</template>
+<script>
+export default {}
+</script>
+<style lang="scss" scoped>
+.table-content {
+  display: flex;
+  flex-direction: column;
+  margin-top: 10px;
+  .header {
+    height: 32px;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    justify-content: space-between;
+    padding: 0 20px;
+    background-color: #e8e8e8;
+    div {
+      height: 32px;
+      line-height: 32px;
+      font-size: 14px;
+    }
+    .gray {
+      color: #4a4a4a;
+    }
+    .blue {
+      color: #1890ff;
+    }
+  }
+  .row {
+    padding-left: 10px;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    border: 1px solid #ebebeb;
+  }
+}
+.table-header {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  height: 42px;
+  background-color: #ebebeb;
+  padding-left: 20px;
+  align-items: center;
+}
+.table-column {
+  color: #9b9b9b;
+  font-size: 14px;
+  padding: 10px;
+  display: flex;
+  align-items: center;
+  &.content-container {
+    display: flex;
+    flex-direction: row;
+    .content-box {
+      display: flex;
+      flex-direction: column;
+      flex-wrap: wrap;
+      flex: 1;
+      margin-left: 10px;
+      .desc {
+        overflow: hidden;
+        text-overflow: ellipsis;
+        display: -webkit-box;
+        -webkit-line-clamp: 2;
+        -webkit-box-orient: vertical;
+        max-height: 40px;
+        margin-bottom: 20px;
+      }
+      .order {
+      }
+    }
+    .image-box {
+      position: relative;
+      width: 72px;
+      height: 72px;
+      .pic {
+        position: absolute;
+        width: 72px;
+        height: 72px;
+      }
+      .box {
+        position: absolute;
+        width: 72px;
+        height: 72px;
+        text-align: center;
+        line-height: 72px;
+        font-size: 14px;
+        color: #9b9b9b;
+        background-color: #ebebeb;
+      }
+    }
+  }
+  &.content-center {
+    justify-content: center;
+  }
+}
+.check-box {
+  margin-right: 10px;
+}
+.flex-1 {
+  flex: 1;
+}
+.flex-2 {
+  flex: 2;
+}
+.flex-3 {
+  flex: 3;
+}
+.flex-4 {
+  flex: 4;
+}
+.flex-5 {
+  flex: 5;
+}
+.flex-6 {
+  flex: 6;
+}
+</style>
