21dff4519c0ef4c9731f15f6b3666a90cc36e67e
张超 <zhangchao@hydee.cn>
2020-02-05 22:50:43 +0800

    提交代码

---
+++ src/views/distribution/pickup-point/add-pickup.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="content_pick">
+    <div class="nav-btn">
+      <el-button type="primary" size="mini" @click="toAdd()">添加提货门店</el-button>
+    </div>
+    <div class="tabel-content">
+      <el-table :data="tableData" border style="width: 100%">
+        <el-table-column prop="date" label="日期" />
+        <el-table-column prop="name" label="姓名" />
+        <el-table-column prop="address" label="地址" />
+        <el-table-column
+          label="操作"
+          align="center"
+        >
+          <template>
+            <el-button type="primary" size="small">编辑</el-button>
+            <el-button type="primary" size="small">启用</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+    <div class="page-box">
+      <el-pagination
+        :current-page="pageInfo.currentPage"
+        :page-sizes="[10, 20, 30, 40]"
+        :page-size="pageInfo.pageSize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="pageInfo.total"
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import { getResevation } from '@/api/reservation-product'
+export default {
+  data() {
+    return {
+      tableData: [
+        {
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },
+        {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄'
+        },
+        {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        },
+        {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }
+      ],
+      pageInfo: {
+        currentPage: 1,
+        pageSize: 10,
+        total: 0
+      }
+    }
+  },
+  created() {
+    this.getStoreData()
+  },
+  methods: {
+    getStoreData() {
+      var parmes = {
+
+      }
+      parmes.currentPage = this.pageInfo.currentPage
+      parmes.pageSize = this.pageInfo.pageSize
+      parmes.merCode = 666666
+      getResevation(parmes).then(res => {
+        console.log(res)
+      })
+    },
+    toAdd() {
+      this.$router.push('/distribution/pickup-point/add-pick')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.content_pick{
+  padding: 10px 21px;height: calc(100vh - 158px);overflow-y: scroll;
+  .nav-btn{
+    text-align: right;height: 40px;line-height: 40px
+  }
+  .page-box{
+    height: 40px;line-height: 40px;margin-top: 21px;text-align: right
+  }
+}
+</style>
--- src/views/distribution/pickup-point/index.vue
+++ src/views/distribution/pickup-point/index.vue
@@ -1,99 +1,105 @@
 <template>
-  <div class="content_pick">
-    <div class="nav-btn">
-      <el-button type="primary" size="mini">添加提货门店</el-button>
+  <div class="add">
+    <div class="product-info">
+      <h4>提货门店信息</h4>
+      <el-form ref="form" :model="form" label-width="110px">
+        <el-form-item label="提货门店名称:">
+          <el-input v-model="form.name" />
+        </el-form-item>
+        <el-form-item label="门店编码:">
+          <el-input v-model="form.name" />
+        </el-form-item>
+        <el-form-item label="门店地址:" style="padding-right:20%">
+          <el-input v-model="form.name" />
+          <el-button type="primary" class="position-btn">定位</el-button>
+        </el-form-item>
+        <div class="map-box">
+          地图
+        </div>
+        <el-form-item label="电话号码:">
+          <el-input v-model="form.name" />
+        </el-form-item>
+      </el-form>
     </div>
-    <div class="tabel-content">
-      <el-table :data="tableData" border style="width: 100%">
-        <el-table-column prop="date" label="日期" />
-        <el-table-column prop="name" label="姓名" />
-        <el-table-column prop="address" label="地址" />
-        <el-table-column
-          label="操作"
-          align="center"
-        >
-          <template>
-            <el-button type="primary" size="small">编辑</el-button>
-            <el-button type="primary" size="small">启用</el-button>
-          </template>
-        </el-table-column>
-      </el-table>
+    <div class="product-img">
+      <h4>门店账号</h4>
+      <el-form ref="form" :model="form" label-width="110px">
+        <el-form-item label="账号设置:">
+          <el-input v-model="form.name" />
+        </el-form-item>
+        <el-form-item label="密码设置:">
+          <el-input v-model="form.name" />
+        </el-form-item>
+      </el-form>
     </div>
-    <div class="page-box">
-      <el-pagination
-        :current-page="pageInfo.currentPage"
-        :page-sizes="[10, 20, 30, 40]"
-        :page-size="pageInfo.pageSize"
-        layout="total, sizes, prev, pager, next, jumper"
-        :total="pageInfo.total"
-        @size-change="handleSizeChange"
-        @current-change="handleCurrentChange"
-      />
+    <div class="submit-box">
+      <el-button type="primary" @click="submitData()">完成添加</el-button>
     </div>
   </div>
 </template>
-
 <script>
-import { getResevation } from '@/api/reservation-product'
 export default {
   data() {
     return {
-      tableData: [
-        {
-          date: '2016-05-02',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        },
-        {
-          date: '2016-05-04',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1517 弄'
-        },
-        {
-          date: '2016-05-01',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1519 弄'
-        },
-        {
-          date: '2016-05-03',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1516 弄'
-        }
-      ],
-      pageInfo: {
-        currentPage: 1,
-        pageSize: 10,
-        total: 0
-      }
+      form: {
+        name: ''
+      },
+      dialogImageUrl: '',
+      dialogVisible: false
     }
   },
-  created() {
-    this.getStoreData()
-  },
   methods: {
-    getStoreData() {
-      var parmes = {
-
-      }
-      parmes.currentPage = this.pageInfo.currentPage
-      parmes.pageSize = this.pageInfo.pageSize
-      parmes.merCode = 666666
-      getResevation(parmes).then(res => {
-        console.log(res)
-      })
+    handleRemove(file, fileList) {
+      console.log(file, fileList)
+    },
+    handlePictureCardPreview(file) {
+      this.dialogImageUrl = file.url
+      this.dialogVisible = true
+    },
+    submitData() { // 提交数据
+      console.log(this.form)
     }
   }
 }
 </script>
-
 <style lang="scss" scoped>
-.content_pick{
-  padding: 10px 21px;height: calc(100vh - 158px);overflow-y: scroll;
-  .nav-btn{
-    text-align: right;height: 40px;line-height: 40px
+.add {
+  padding: 10px 61px;height: calc(100vh - 158px);overflow-y: scroll;
+  .product-img, .product-info, .product-rules{
+    padding: 20px 0;
+    h4{
+      height: 30px;line-height: 30px;font-weight: 600;font-size: 16px;
+      margin-bottom: 21px;
+      .tips-yuyue{
+        font-size:14px;color:rgba(0,0,0,0.45);
+      }
+    }
+    form{
+      padding-left: 20%; width:80%;
+      .position-btn{
+        position: absolute;right: 0;transform: translateX(110%)
+      }
+      .map-box{
+        width: calc(100% - 110px);
+        margin: 20px 0;
+        margin-left: 110px;
+        height: 200px;
+        border: 1px solid #eee
+      }
+    }
+    .tips{
+      font-size:14px;
+      font-weight:400;
+      color:rgba(0,0,0,0.45);
+      line-height:20px;
+      margin-top: 10px
+    }
   }
-  .page-box{
-    height: 40px;line-height: 40px;margin-top: 21px;text-align: right
+  .product-rules{
+    border-top: 1px solid #eee ;border-bottom: 1px solid #eee
+  }
+  .submit-box{
+    text-align: center;margin-top: 20px
   }
 }
 </style>
