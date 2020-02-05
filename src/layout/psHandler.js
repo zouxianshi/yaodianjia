9d94dca456c48df1caa6570a7c0a732496685bf9
gesineng@hydee.cn <gesineng@hydee.cn>
2020-02-05 18:45:10 +0800

    feature:紧急项目接口代理

--- src/layout/psHandler.js
+++ src/layout/psHandler.js
@@ -1,114 +1,114 @@
 /* eslint-disable */
 
-import _ from "lodash";
-import store from "@/store";
-const ps = () => new InnerCtor();
+import _ from 'lodash'
+import store from '@/store'
+const ps = () => new InnerCtor()
 
 class InnerCtor {
   constructor() {
-    this.p = {};
+    this.p = {}
   }
   // 客服消息按钮权限
   showMsgBtn() {
-    return this.get("staff.online-ask");
+    return this.get('staff.online-ask')
   }
   // 订单立即发货按钮权限
   showSendGoodsBtn() {
     //立即发货鉴权
-    return this.get("order.order-all.immediate-delivery");
+    return this.get('order.order-all.immediate-delivery')
   }
   navList() {
     return [
       {
-        name: "首页",
-        icon: "iconshouye2",
-        path: "/home",
+        name: '首页',
+        icon: 'iconshouye2',
+        path: '/home',
         disabled: true,
         children: []
       },
       {
-        name: "商品中心",
-        icon: "icongoods",
-        path: "",
-        disabled: this.get("commodity"),
+        name: '商品中心',
+        icon: 'icongoods',
+        path: '',
+        disabled: this.get('commodity'),
         children: [
           {
-            name: "商品库",
-            path: "/goods-manage/depot",
-            disabled: this.get("commodity.commodity-lib")
+            name: '商品库',
+            path: '/goods-manage/depot',
+            disabled: this.get('commodity.commodity-lib')
           },
           {
             name: `自建新品`,
-            path: "/goods-manage/apply",
-            disabled: this.get("commodity.commodity-self")
+            path: '/goods-manage/apply',
+            disabled: this.get('commodity.commodity-self')
           },
           {
-            name: "新品申请记录",
-            path: "/goods-manage/apply-record",
-            disabled: this.get("commodity.commodity-new-record")
+            name: '新品申请记录',
+            path: '/goods-manage/apply-record',
+            disabled: this.get('commodity.commodity-new-record')
           },
           {
-            name: "新品审核",
-            path: "/goods-manage/examine",
-            disabled: this.get("commodity.commodity-check")
+            name: '新品审核',
+            path: '/goods-manage/examine',
+            disabled: this.get('commodity.commodity-check')
           },
           {
             name: `自定义分组`,
-            path: "/goods-manage/group",
-            disabled: this.get("commodity.commodity-group")
+            path: '/goods-manage/group',
+            disabled: this.get('commodity.commodity-group')
           },
           {
             name: `门店商品管理`,
-            path: "/goods-manage/store-goods",
-            disabled: this.get("commodity.commodity-store")
+            path: '/goods-manage/store-goods',
+            disabled: this.get('commodity.commodity-store')
           },
           {
             name: `组合商品`,
-            path: "/goods-manage/constitute-goods",
-            disabled: this.get("commodity.commodity-combine")
+            path: '/goods-manage/constitute-goods',
+            disabled: this.get('commodity.commodity-combine')
           }
         ]
       },
       {
-        name: "商城装修",
-        icon: "icongoods",
-        path: "",
-        disabled: this.get("yaodianjia-wxmall"),
+        name: '商城装修',
+        icon: 'icongoods',
+        path: '',
+        disabled: this.get('yaodianjia-wxmall'),
         children: [
           {
-            name: "主页设置",
-            path: "/wxmall/home-set",
-            disabled: this.get("yaodianjia-wxmall.yaodianjia-wxmall-home-set")
+            name: '主页设置',
+            path: '/wxmall/home-set',
+            disabled: this.get('yaodianjia-wxmall.yaodianjia-wxmall-home-set')
           },
           {
-            name: "首页轮播图",
-            path: "/wxmall/banner",
-            disabled: this.get("yaodianjia-wxmall.yaodianjia-wxmall-banner")
+            name: '首页轮播图',
+            path: '/wxmall/banner',
+            disabled: this.get('yaodianjia-wxmall.yaodianjia-wxmall-banner')
           },
           {
-            name: "首页公告栏",
-            path: "/wxmall/notice",
-            disabled: this.get("yaodianjia-wxmall.yaodianjia-wxmall-notice")
+            name: '首页公告栏',
+            path: '/wxmall/notice',
+            disabled: this.get('yaodianjia-wxmall.yaodianjia-wxmall-notice')
           },
           {
-            name: "分类广告位",
-            path: "/wxmall/ad-position",
+            name: '分类广告位',
+            path: '/wxmall/ad-position',
             disabled: this.get(
-              "yaodianjia-wxmall.yaodianjia-wxmall-ad-position"
+              'yaodianjia-wxmall.yaodianjia-wxmall-ad-position'
             )
           }
         ]
       },
       {
-        name: "订单中心",
-        icon: "icongoods",
-        path: "/orders-manage",
-        disabled: this.get("order"),
+        name: '订单中心',
+        icon: 'icongoods',
+        path: '/orders-manage',
+        disabled: this.get('order'),
         children: [
           {
             name: `所有订单`,
-            path: "/orders-manage/list",
-            disabled: this.get("order.order-all")
+            path: '/orders-manage/list',
+            disabled: this.get('order.order-all')
           },
           // {
           //   name: `订单详情`,
@@ -117,63 +117,63 @@ class InnerCtor {
           // },
           {
             name: `订单设置`,
-            path: "/orders-manage/settings",
-            disabled: this.get("order.order-set")
+            path: '/orders-manage/settings',
+            disabled: this.get('order.order-set')
           }
         ]
       },
 
       {
-        name: "营销中心",
-        icon: "icongoods",
-        path: "",
-        disabled: this.get("marketing"),
+        name: '营销中心',
+        icon: 'icongoods',
+        path: '',
+        disabled: this.get('marketing'),
         children: [
           {
-            name: "新建活动",
-            path: "/marketing/activity/create",
-            disabled: this.get("marketing.marketing-create")
+            name: '新建活动',
+            path: '/marketing/activity/create',
+            disabled: this.get('marketing.marketing-create')
           },
           {
-            name: "活动管理",
-            path: "/marketing/activity",
-            disabled: this.get("marketing.marketing-manager")
+            name: '活动管理',
+            path: '/marketing/activity',
+            disabled: this.get('marketing.marketing-manager')
           }
         ]
       },
       {
-        name: "客服中心",
-        icon: "icongoods",
-        path: "/customerService",
-        disabled: this.get("staff"),
+        name: '客服中心',
+        icon: 'icongoods',
+        path: '/customerService',
+        disabled: this.get('staff'),
         children: [
           {
-            name: "客服列表",
-            path: "/customerService/list",
-            disabled: this.get("staff.staff-list")
+            name: '客服列表',
+            path: '/customerService/list',
+            disabled: this.get('staff.staff-list')
           },
           {
-            name: "快捷消息",
-            path: "/customerService/msgSettings",
-            disabled: this.get("staff.quick-message-set")
+            name: '快捷消息',
+            path: '/customerService/msgSettings',
+            disabled: this.get('staff.quick-message-set')
           },
           {
-            name: "在线咨询",
-            path: "/customerService/consultation",
-            disabled: this.get("staff.online-ask")
+            name: '在线咨询',
+            path: '/customerService/consultation',
+            disabled: this.get('staff.online-ask')
           },
           {
-            name: "消息记录",
-            path: "/customerService/historyMsg",
-            disabled: this.get("staff.message-record")
+            name: '消息记录',
+            path: '/customerService/historyMsg',
+            disabled: this.get('staff.message-record')
           }
         ]
       },
       {
-        name: "设置中心",
-        icon: "icongoods",
-        path: "",
-        disabled: this.get("set"),
+        name: '设置中心',
+        icon: 'icongoods',
+        path: '',
+        disabled: this.get('set'),
         children: [
           /*{
             name: '所有门店',
@@ -181,34 +181,34 @@ class InnerCtor {
             disabled: this.get('merchant-org.merchant-org-org'),
           },*/
           {
-            name: "连锁设置",
-            path: "/chainSetting",
-            disabled: this.get("set.merchant-set"),
+            name: '连锁设置',
+            path: '/chainSetting',
+            disabled: this.get('set.merchant-set'),
             children: []
           },
           {
-            name: "上线门店",
-            path: "/storeSetting/setting",
-            disabled: this.get("set.store-up")
+            name: '上线门店',
+            path: '/storeSetting/setting',
+            disabled: this.get('set.store-up')
           },
           {
-            name: "门店配送",
-            path: "/storeSetting/deliverySetting",
-            disabled: this.get("set.store-deliver")
+            name: '门店配送',
+            path: '/storeSetting/deliverySetting',
+            disabled: this.get('set.store-deliver')
           },
           {
-            name: "内部链接",
-            path: "/internalLink",
-            disabled: this.get("set.inner-link"),
+            name: '内部链接',
+            path: '/internalLink',
+            disabled: this.get('set.inner-link'),
             children: []
           }
         ]
       },
 
       {
-        name: "预约商品",
-        icon: "icongoods",
-        path: "",
+        name: '预约商品',
+        icon: 'icongoods',
+        path: '',
         // disabled: this.get("distribution"),
         disabled: true,
         children: [
@@ -224,57 +224,57 @@ class InnerCtor {
           //   children: []
           // },
           {
-            name: "预约单管理",
-            path: "/distribution/order-manage",
+            name: '预约单管理',
+            path: '/distribution/order-manage',
             disabled: true
-          },
-          {
-            name: "核销管理",
-            path: "/distribution/write-off",
-            disabled: true
-          },
-          {
-            name: "小程序设置",
-            path: "/distribution/weapp-setting",
-            disabled: true,
-            children: []
           }
+          // {
+          //   name: "核销管理",
+          //   path: "/distribution/write-off",
+          //   disabled: true
+          // },
+          // {
+          //   name: "小程序设置",
+          //   path: "/distribution/weapp-setting",
+          //   disabled: true,
+          //   children: []
+          // }
         ]
       }
-    ];
+    ]
   }
   handlerPs() {
-    let p = {};
-    console.warn("resList _____________________ : ", store.state.user.resList);
+    let p = {}
+    console.warn('resList _____________________ : ', store.state.user.resList)
     _.map(store.state.user.resList, v => {
       _.map(v.children, v1 => {
         p[v.rePath] = _.assign(p[v.rePath], {
           [v1.rePath]: _.map(v1.children, v2 => v2.rePath)
-        });
-      });
-    });
-    this.p = p;
-    return p;
+        })
+      })
+    })
+    this.p = p
+    return p
   }
 
   get(str) {
-    let p = this.p;
+    let p = this.p
     if (_.isEmpty(p)) {
-      p = this.handlerPs();
+      p = this.handlerPs()
     }
-    const strArr = _.split(str, ".");
-    const leven1Code = strArr[0];
-    const leven2Code = strArr[1];
-    const leven3Code = strArr[2] || "";
+    const strArr = _.split(str, '.')
+    const leven1Code = strArr[0]
+    const leven2Code = strArr[1]
+    const leven3Code = strArr[2] || ''
     switch (strArr.length) {
       case 1:
-        return _.has(p, leven1Code);
+        return _.has(p, leven1Code)
       case 2:
-        return _.has(p[leven1Code], leven2Code);
+        return _.has(p[leven1Code], leven2Code)
       default:
-        return p[leven1Code][leven2Code].includes(leven3Code);
+        return p[leven1Code][leven2Code].includes(leven3Code)
     }
   }
 }
 
-export default ps();
+export default ps()
--- src/views/distribution/order-manage/index.vue
+++ src/views/distribution/order-manage/index.vue
@@ -2,12 +2,37 @@
   <div class="container">
     <div class="header-panel">
       <div class="box">
-        <div class="item" :class="'active'" @click="setDialogContent('')">
+        <div
+          class="item frist"
+          :class="navItem === 'all' && 'active'"
+          @click="changeTableItem('all')"
+        >
           全部
         </div>
-        <div class="item">待到货</div>
-        <div class="item">待核销</div>
-        <div class="item">已完成</div>
+        <div class="cutoff" />
+        <div
+          class="item"
+          :class="navItem === 'receive' && 'active'"
+          @click="changeTableItem('receive')"
+        >
+          待到货
+        </div>
+        <div class="cutoff" />
+        <div
+          class="item"
+          :class="navItem === 'off' && 'active'"
+          @click="changeTableItem('off')"
+        >
+          待核销
+        </div>
+        <div class="cutoff" />
+        <div
+          class="item last"
+          :class="navItem === 'finish' && 'active'"
+          @click="changeTableItem('finish')"
+        >
+          已完成
+        </div>
       </div>
       <el-select
         v-model="value"
@@ -22,15 +47,26 @@
           :value="item.value"
         />
       </el-select>
-      <el-input
-        v-model="input2"
-        placeholder="请输入内容"
-        prefix-icon="el-icon-search"
-        class="search"
-      />
+      <div class="search-container">
+        <div class="search-item" @keyup.enter="forSearch()">
+          <el-input
+            v-model="orderNum"
+            placeholder="请输入预约单号搜索"
+            prefix-icon="el-icon-search"
+            class="search"
+          />
+        </div>
+        <div class="search-btns" style="margin-left:10px;">
+          <el-button
+            type="primary"
+            size="small"
+            @click.stop="forSearch()"
+          >搜 索</el-button>
+        </div>
+      </div>
     </div>
     <div class="table-panel">
-      <table-card />
+      <table-card @button-click="setDialogContent" />
     </div>
     <div class="pagination-panel">
       <el-pagination
@@ -51,11 +87,7 @@
       <el-table :data="dialogContent">
         <el-table-column property="pic" label="商品图片" width="150">
           <div class="dialog-pic">暂无图片</div></el-table-column>
-        <el-table-column
-          property="name"
-          label="商品名称/编号"
-          width="200"
-        />
+        <el-table-column property="name" label="商品名称/编号" width="200" />
         <el-table-column property="price" label="单价" />
         <el-table-column property="count" label="数量" />
       </el-table>
@@ -80,7 +112,7 @@
       <div
         style="display:flex;flex-direction:row;margin-top:40px;justify-content: center;"
       >
-        <el-button>取消</el-button>
+        <el-button @click="closeDialog">取消</el-button>
         <el-button type="primary">确认</el-button>
       </div>
     </el-dialog>
@@ -89,23 +121,47 @@
 
 <script>
 import tableCard from './table-card/index.vue'
+import DistributionService from '@/api/distributionService'
 export default {
   components: { tableCard },
   data() {
     return {
       dialogVisible: false,
       dialogTitle: '',
-      dialogContent: []
+      dialogContent: [],
+      navItem: 'all',
+      orderNum: ''
     }
   },
+  watch: {
+    orderNum(newValue) {
+      this.orderNum = newValue
+    }
+  },
+  mounted() {
+    const data = DistributionService.queryOrderListByType()
+    console.log('recvicesfdsfsfdsf ', data)
+  },
   methods: {
-    setDialogContent(type) {
+    /* 搜索 */
+    forSearch() {
+      console.log('hey : search bar ')
+    },
+    /* 切换展示数据类型 */
+    changeTableItem(type) {
+      this.navItem = type
+    },
+    /* table-card 组件按钮点击 */
+    setDialogContent() {
       this.dialogVisible = true
-      if (type === 'writeoff') {
-        this.dialogTitle = '确认核销'
-      } else {
-        this.dialogTitle = '确认到货 (签收后预约单进入核销状态，请提醒用户上门)'
-      }
+      // if (type === 'writeoff') {
+      //   this.dialogTitle = '确认核销'
+      // } else {
+      //   this.dialogTitle = '确认到货 (签收后预约单进入核销状态，请提醒用户上门)'
+      // }
+    },
+    closeDialog() {
+      this.dialogVisible = false
     }
   }
 }
@@ -122,6 +178,11 @@ export default {
     display: flex;
     flex-direction: row;
     align-items: center;
+    .search-container {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+    }
     .search {
       width: 284px;
       margin-left: 44px;
@@ -132,6 +193,10 @@ export default {
       flex-direction: row;
       border: 1px solid #d7d7d7;
       flex-shrink: 0;
+      .cutoff {
+        width: 1px;
+        background-color: #d7d7d7;
+      }
       .item {
         padding: 12px;
         font-size: 14px;
@@ -139,7 +204,12 @@ export default {
         color: #4a4a4a;
         cursor: pointer;
         display: inline-block;
-
+        &.frist {
+          border-radius: 6px 0 0 6px;
+        }
+        &.last {
+          border-radius: 0 6px 6px 0;
+        }
         &.active {
           color: #fff;
           background: #157de8;
@@ -158,20 +228,6 @@ export default {
     font-size: 14px;
     color: #9b9b9b;
     background-color: #ebebeb;
-  }
-  #content-panel {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    .default {
-      color: #000000;
-      font-size: 14px;
-    }
-    .txt {
-      color: #000000;
-      font-size: 14px;
-      margin-left: 20px;
-    }
   }
 }
 </style>
