ca3a91ed71ffc7774649cfa7fd059dc5cf9a979e
张超 <zhangchao@hydee.cn>
2020-02-05 15:20:41 +0800

    提交代码

--- src/api/reservation-product.js
+++ src/api/reservation-product.js
@@ -1,11 +1,11 @@
 import request from '@/utils/request'
-import config from '@/utils/config'
+// import config from '@/utils/config'
 
 // 获取预约商品列表
 export function getResevation(params) {
   return request({
-    url: `${config.merGoods}/1.0/b/store/_query`,
-    method: 'POST',
+    url: `/1.0/b/store/_query`,
+    method: 'post',
     data: params
   })
 }
