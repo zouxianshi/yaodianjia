3dd2c0b7958b47a4702af1351d4ac3627226fbdb
zengrong <z@zxr.me>
2020-02-08 20:13:34 +0800

    commit

--- src/router/modules/router-r.js
+++ src/router/modules/router-r.js
@@ -1,7 +1,25 @@
 import Layout from '@/layout'
 const router = [
+  // {
+  //   path: '/',
+  //   component: Layout,
+  //   redirect: '/chainSetting',
+  //   meta: {
+  //     title: '设置中心',
+  //     icon: 'goods'
+  //   },
+  //   children: [
+  //     {
+  //       path: 'chainSetting',
+  //       component: () => import('@/views/chainSetting/index'),
+  //       name: 'chainSetting',
+  //       hidden: true,
+  //       meta: { title: '连锁设置', icon: 'chainSetting' }
+  //     }
+  //   ]
+  // },
   {
-    path: '/',
+    path: '/storeSetting',
     component: Layout,
     redirect: '/chainSetting',
     meta: {
@@ -15,31 +33,20 @@ const router = [
         name: 'chainSetting',
         hidden: true,
         meta: { title: '连锁设置', icon: 'chainSetting' }
-      }
-    ]
-  },
-  {
-    path: '/storeSetting',
-    component: Layout,
-    redirect: '/chainSetting',
-    meta: {
-      title: '设置中心',
-      icon: 'goods'
-    },
-    children: [
-      {
-        path: 'index',
-        component: () => import('@/views/storeSetting/index'),
-        name: 'index',
-        hidden: true,
-        meta: { title: '所有门店', icon: 'storeSetting' }
       },
+      // {
+      //   path: 'index',
+      //   component: () => import('@/views/storeSetting/index'),
+      //   name: 'index',
+      //   hidden: true,
+      //   meta: { title: '所有门店', icon: 'storeSetting' }
+      // },
       {
         path: 'setting',
         component: () => import('@/views/storeSetting/setting'),
         name: 'setting',
         hidden: true,
-        meta: { title: '门店设置', icon: 'storeSetting' }
+        meta: { title: '上线门店', icon: 'storeSetting' }
       },
       {
         path: 'deliverySetting',
@@ -47,18 +54,7 @@ const router = [
         name: 'deliverySetting',
         hidden: true,
         meta: { title: '门店配送', icon: 'storeSetting' }
-      }
-    ]
-  },
-  {
-    path: '/',
-    component: Layout,
-    redirect: '/chainSetting',
-    meta: {
-      title: '设置中心',
-      icon: 'goods'
-    },
-    children: [
+      },
       {
         path: 'internalLink',
         component: () => import('@/views/internalLink/index'),
@@ -68,6 +64,24 @@ const router = [
       }
     ]
   },
+  // {
+  //   path: '/',
+  //   component: Layout,
+  //   redirect: '/chainSetting',
+  //   meta: {
+  //     title: '设置中心',
+  //     icon: 'goods'
+  //   },
+  //   children: [
+  //     {
+  //       path: 'internalLink',
+  //       component: () => import('@/views/internalLink/index'),
+  //       name: 'internalLink',
+  //       hidden: true,
+  //       meta: { title: '内部链接', icon: 'internalLink' }
+  //     }
+  //   ]
+  // },
   { path: '*', redirect: '/404', hidden: true, meta: { title: '404' }}
 ]
 
