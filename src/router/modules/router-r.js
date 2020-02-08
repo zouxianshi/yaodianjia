a51fea2d5fbdaf6c0cbb2eebeacd29eb924aae4a
zengrong <z@zxr.me>
2020-02-08 20:26:19 +0800

    commit

--- src/router/modules/router-r.js
+++ src/router/modules/router-r.js
@@ -1,23 +1,5 @@
 import Layout from '@/layout'
 const router = [
-  // {
-  //   path: '/',
-  //   component: Layout,
-  //   redirect: '/chainSetting',
-  //   meta: {
-  //     title: '设置中心',
-  //     icon: 'goods'
-  //   },
-  //   children: [
-  //     {
-  //       path: 'chainSetting',
-  //       component: () => import('@/views/chainSetting/index'),
-  //       name: 'chainSetting',
-  //       hidden: true,
-  //       meta: { title: '连锁设置', icon: 'chainSetting' }
-  //     }
-  //   ]
-  // },
   {
     path: '/storeSetting',
     component: Layout,
@@ -34,13 +16,6 @@ const router = [
         hidden: true,
         meta: { title: '连锁设置', icon: 'chainSetting' }
       },
-      // {
-      //   path: 'index',
-      //   component: () => import('@/views/storeSetting/index'),
-      //   name: 'index',
-      //   hidden: true,
-      //   meta: { title: '所有门店', icon: 'storeSetting' }
-      // },
       {
         path: 'setting',
         component: () => import('@/views/storeSetting/setting'),
@@ -64,24 +39,6 @@ const router = [
       }
     ]
   },
-  // {
-  //   path: '/',
-  //   component: Layout,
-  //   redirect: '/chainSetting',
-  //   meta: {
-  //     title: '设置中心',
-  //     icon: 'goods'
-  //   },
-  //   children: [
-  //     {
-  //       path: 'internalLink',
-  //       component: () => import('@/views/internalLink/index'),
-  //       name: 'internalLink',
-  //       hidden: true,
-  //       meta: { title: '内部链接', icon: 'internalLink' }
-  //     }
-  //   ]
-  // },
   { path: '*', redirect: '/404', hidden: true, meta: { title: '404' }}
 ]
 
