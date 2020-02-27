d89ebc6cacfa423b317620bc23160a6ed1ee21f5
hezhixing <314290737@qq.com>
2020-02-27 16:52:32 +0800

    处理商城首页设置上传接口混入电商云代码问题

--- src/views/wxmall/home-set.vue
+++ src/views/wxmall/home-set.vue
@@ -848,7 +848,7 @@ export default {
       return this.$store.state.user.merCode || ''
     },
     upLoadUrl() {
-      return `${this.uploadFileURL}${config.merGoods}/1.0/ds/baseinfo/synDeliveryStore`
+      return `${this.uploadFileURL}/${config.merGoods}/1.0/file/_upload?merCode=${this.merCode}`
     },
     headers() {
       return { 'Authorization': this.$store.getters.token }
