f8b0bad5e1ccc16051b9b4a6632ba1dcc41f7278
quanquan <quanquansy@gmail.com>
2020-04-01 16:22:55 +0800

    fix: 增加渠道管理微信会员卡

---
+++ src/views/channel/wxOffAccount/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="index-model">
+    index
+  </div>
+</template>
+<script>
+export default {
+  name: 'Index',
+  components: {},
+  props: {},
+  data() {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  beforeCreate() {
+  },
+  created() {
+  },
+  beforeMount() {
+  },
+  mounted() {
+  },
+  beforeUpdate() {
+  },
+  updated() {
+  },
+  methods: {},
+  beforeDestroy() {
+  },
+  destroyed() {
+  }
+}
+</script>
+
+<style lang="scss" rel="stylesheet/scss">
+  .index-model {
+
+  }
+</style>
