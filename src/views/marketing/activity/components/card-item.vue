4a35d42cb13e94d32383ac62e252f15707a38493
weiying lv <421363354@qq.com>
2020-04-09 17:00:06 +0800

    feat: 新增限时特惠和限时秒杀菜单

--- src/views/marketing/activity/components/card-item.vue
+++ src/views/marketing/activity/components/card-item.vue
@@ -8,7 +8,9 @@
     <div class="activity-bottom">
       <div class="activity-header">
         <div class="title">{{ item.lable }}</div>
-        <div class="sub-title">{{ item.desc || '' }}</div>
+        <el-tooltip class="item" effect="dark" :content="item.desc" placement="top-start">
+          <div class="sub-title">{{ item.desc || '' }}</div>
+        </el-tooltip>
       </div>
       <div class="action">
         <el-button type="text" class="button">活动列表</el-button>
@@ -29,31 +31,36 @@ export default {
 
 <style lang="scss">
 .activity-card {
-    .el-card__body {
-      padding: 0;
-    }
-    .image {
-      width: 100%;
-      display: block;
-      // height: 183px;
-    }
-    .activity-bottom {
-      padding: 14px;
-      padding-bottom: 6px;
-      .activity-header {
-        .title {
-          font-size: 16px;
-          color: rgba(0,0,0,.85);
-        }
-        .sub-title {
-          color: rgba(0,0,0,.65);
-          font-size: 10px;
-          line-height: 24px;
-        }
-      }
-      // .action {
-      //   text-align: right;
-      // }
-    }
+  .el-card__body {
+    padding: 0;
   }
+  .image {
+    width: 100%;
+    display: block;
+    // height: 183px;
+  }
+  .activity-bottom {
+    padding: 14px;
+    padding-bottom: 6px;
+    .activity-header {
+      .title {
+        font-size: 16px;
+        color: rgba(0, 0, 0, 0.85);
+      }
+      .sub-title {
+        color: rgba(0, 0, 0, 0.65);
+        font-size: 10px;
+        line-height: 24px;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        display: -webkit-box;
+        -webkit-line-clamp: 2;
+        -webkit-box-orient: vertical;
+      }
+    }
+    // .action {
+    //   text-align: right;
+    // }
+  }
+}
 </style>
