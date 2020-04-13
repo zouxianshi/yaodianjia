d130fd9184c6bad48189d3dfac51279ff8a79303
quanquan <quanquansy@gmail.com>
2020-04-13 15:16:48 +0800

    feat(主页设置增加拖拽):

--- src/views/mall/homeSettings/_source/componentBar.vue
+++ src/views/mall/homeSettings/_source/componentBar.vue
@@ -1,17 +1,64 @@
 <template>
   <div class="component-bar-model">
-    component-bar
+    <div class="cbm-title">
+      <span>组件选择</span>
+    </div>
+    <div class="cbm-draggable">
+      <v-draggable v-model="dragComponent" draggable=".item" v-bind="dragOptions">
+        <transition-group type="transition" :name="'flip-list'">
+          <span v-for="(el,i) in dragComponent" :key="i" class="item">
+            <el-button size="small">{{ el.name }}</el-button>
+          </span>
+        </transition-group>
+      </v-draggable>
+    </div>
+    <div class="clearfix" />
   </div>
 </template>
 <script>
+import vDraggable from 'vuedraggable'
+const dragComponent = [
+  {
+    name: '导航栏',
+    code: 'navigation'
+  },
+  {
+    name: '商品',
+    code: 'commodity'
+  },
+  {
+    name: '标题',
+    code: 'title'
+  },
+  {
+    name: '广告图',
+    code: 'advertise'
+  },
+  {
+    name: '为你推荐',
+    code: '为你推荐'
+  }
+]
+
 export default {
   name: 'ComponentBar',
-  components: {},
+  components: { vDraggable },
   props: {},
   data() {
-    return {}
+    return {
+      dragComponent
+    }
   },
-  computed: {},
+  computed: {
+    dragOptions() {
+      return {
+        animation: 0,
+        group: 'description',
+        disabled: false,
+        ghostClass: 'ghost'
+      }
+    }
+  },
   watch: {},
   beforeCreate() {
   },
@@ -36,5 +83,24 @@ export default {
 <style lang="scss" rel="stylesheet/scss">
   .component-bar-model {
     height: 50px;
+    .cbm-title,.cbm-draggable {
+      float: left;
+    }
+    .cbm-title {
+      line-height: 50px;
+      width: 80px;
+      text-align: right;
+      margin-right: 16px;
+      span {
+        font-size: 14px;
+      }
+    }
+    .cbm-draggable {
+      padding-top: 10px;
+      width: calc(100% - 96px);
+      .item {
+        margin-right: 10px;
+      }
+    }
   }
 </style>
--- src/views/mall/homeSettings/_source/viewArea/viewArea.vue
+++ src/views/mall/homeSettings/_source/viewArea/viewArea.vue
@@ -9,20 +9,49 @@
     <!--公告-->
     <m-notice />
 
+    <div class="vam-draggable">
+      <v-draggable v-model="dragComponent" draggable=".item" v-bind="dragOptions">
+        <transition-group type="transition" :name="'va-list'">
+          <span v-for="(el,i) in dragComponent" :key="i" class="item">
+            <el-button size="small">{{ el.name }}</el-button>
+          </span>
+        </transition-group>
+      </v-draggable>
+    </div>
   </div>
 </template>
 <script>
+import vDraggable from 'vuedraggable'
 import mHeader from './header'
 import mBanner from './banner'
 import mNotice from './notice'
+
+const dragComponent = [
+  {
+    name: '导航栏',
+    code: 'navigation'
+  }
+]
+
 export default {
   name: 'ViewArea',
-  components: { mHeader, mBanner, mNotice },
+  components: { mHeader, mBanner, mNotice, vDraggable },
   props: {},
   data() {
-    return {}
+    return {
+      dragComponent
+    }
   },
-  computed: {},
+  computed: {
+    dragOptions() {
+      return {
+        animation: 0,
+        group: 'description',
+        disabled: false,
+        ghostClass: 'ghost'
+      }
+    }
+  },
   watch: {},
   beforeCreate() {
   },
@@ -47,5 +76,8 @@ export default {
 <style lang="scss" rel="stylesheet/scss">
   .view-area-model {
     height: 1200px;
+    .vam-draggable {
+
+    }
   }
 </style>
