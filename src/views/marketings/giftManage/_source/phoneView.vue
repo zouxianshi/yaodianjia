67289886e78052efe753570215c829e369545bb5
张超 <zhangchao@hydee.cn>
2020-04-13 17:57:41 +0800

    feat: 新增优惠券

--- src/views/marketings/giftManage/_source/phoneView.vue
+++ src/views/marketings/giftManage/_source/phoneView.vue
@@ -1,19 +1,59 @@
 <template>
   <div class="phone-view-modal">
     <div class="header">
-      <span> 返回 </span>
+      <span> &emsp;‹返回 </span>
+      <span class="title">优惠券</span>
+      <span class="title">...&emsp;</span>
+    </div>
+    <div class="body">
+      <div class="content">
+        <div class="algin-center">
+          <img src="">
+          <div>中智大药房</div>
+          <div>双十一阿胶9折优惠券</div>
+          <div><el-button type="success" size="nimi">立即使用</el-button></div>
+        </div>
+        <div class="algin-left">
+          <div>使用条件：订单金额不限制，全场通用</div>
+          <div>可用时间：自领取1天后3天有效</div>
+          <div>可用门店：部分门店</div>
+          <div>使用须知：</div>
+          <div class="codeImg">二维码</div>
+          <div class="code">券码：12345678</div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 <script>
-export default {}
+export default {
+  name: 'PhoneView'
+}
 </script>
-<style lang="scss">
+<style lang="scss" scoped>
 .phone-view-modal {
-  border: 1px solid #eee;
+  border: 1px solid #aaa;
   .header {
     background-color: #333;
     color: #fff;
+    display: flex;
+    justify-content: space-between;
+    height: 40px;line-height:40px;
+    .title{
+      font-weight: bold;font-size: 16px;
+    }
+  }
+  .body{
+    height: 400px;padding: 20px;
+    .content{
+      border: 1px solid #aaa;height: 100%;
+      .algin-center{
+        text-align: center;width: 100%;
+        img{
+          height: 80px;width: 80px;border-radius: 50%;border:1px solid #eee;
+        }
+      }
+    }
   }
 }
 </style>
--- src/views/marketings/giftManage/discount/index.vue
+++ src/views/marketings/giftManage/discount/index.vue
@@ -1,40 +1,51 @@
 <template>
   <div class="discount-index-model">
-    创建折扣券
+    <div class="content">
+      <div class="discount-content-l">
+        <mPhoneView />
+      </div>
+      <div class="discount-content-r">
+        <el-steps :active="active" finish-status="success" simple>
+          <el-step title="基本信息" />
+          <el-step title="使用规则" />
+          <el-step title="卡券同步设置" />
+        </el-steps>
+        <el-button @click="next">123</el-button>
+      </div>
+    </div>
   </div>
 </template>
 <script>
+import mPhoneView from '../_source/phoneView'
 export default {
   name: 'DiscountIndex',
-  components: {},
-  props: {},
+  components: {
+    mPhoneView
+  },
   data() {
-    return {}
+    return {
+      active: 0
+    }
   },
-  computed: {},
-  watch: {},
-  beforeCreate() {
-  },
-  created() {
-  },
-  beforeMount() {
-  },
-  mounted() {
-  },
-  beforeUpdate() {
-  },
-  updated() {
-  },
-  beforeDestroy() {
-  },
-  destroyed() {
-  },
-  methods: {}
+  methods: {
+    next() {
+      if (this.active++ > 2) this.active = 0
+    }
+  }
 }
 </script>
 
-<style lang="scss" rel="stylesheet/scss">
+<style lang="scss" rel="stylesheet/scss" scoped>
   .discount-index-model {
-
+    padding: 20px;
+    .content{
+      display: flex;justify-content: space-between;
+      .discount-content-l{
+        flex: 0 0 30%;
+      }
+      .discount-content-r{
+        flex: 0 0 70%;padding-left: 24px;
+      }
+    }
   }
 </style>
