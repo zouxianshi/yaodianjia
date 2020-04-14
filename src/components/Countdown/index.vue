f3889738df5f79057cd742e37b9b68113834c804
weiying lv <421363354@qq.com>
2020-04-14 17:06:06 +0800

    fix:  倒计时修复

--- src/components/Countdown/index.vue
+++ src/components/Countdown/index.vue
@@ -30,6 +30,7 @@ export default {
      */
     _CountDownLoop() {
       const timer_offset = new Date(this.time) - new Date()
+      console.log('---------', timer_offset)
       // 如果结束时间戳减去当前时间时间戳小于等于0则设置倒计时结束标识为true
       if (timer_offset <= 0) {
         cancelAnimationFrame(this.resId)
@@ -40,24 +41,39 @@ export default {
     },
     // 格式ihua
     formatTime(leftTime) {
-      const d = parseInt(leftTime / 1000 / 60 / 60 / 24)// 总毫秒除以一天的毫秒 得到相差的天数
-      let h = parseInt(leftTime / 1000 / 60 / 60 - (24 * d)) // 然后取完天数之后的余下的毫秒数再除以每小时的毫秒数得到小时
-      let m = parseInt(leftTime / 1000 / 60 - (24 * 60 * d) - (60 * h))// 减去天数和小时数的毫秒数剩下的毫秒，再除以每分钟的毫秒数，得到分钟数
-      let s = parseInt(leftTime / 1000 - (24 * 60 * 60 * d) - (60 * 60 * h) - (60 * m)) // 得到最后剩下的毫秒数除以1000 就是秒数，再剩下的毫秒自动忽略即可
-      let hm = Math.floor((leftTime - (24 * 60 * 60 * 1000 * d) - (60 * 60 * 1000 * h) - (60 * 1000 * m) - (s * 1000)) / 10)
-      if (hm < 10) {
-        hm = '0' + hm
+      console.log('formatTime', leftTime)
+      if (leftTime <= 0) {
+        return '00:00:00'
+      } else {
+        const d = Math.floor(leftTime / 1000 / 60 / 60 / 24) // 总毫秒除以一天的毫秒 得到相差的天数
+        let h = Math.floor(leftTime / 1000 / 60 / 60 - 24 * d) // 然后取完天数之后的余下的毫秒数再除以每小时的毫秒数得到小时
+        let m = Math.floor(leftTime / 1000 / 60 - 24 * 60 * d - 60 * h) // 减去天数和小时数的毫秒数剩下的毫秒，再除以每分钟的毫秒数，得到分钟数
+        let s = Math.floor(
+          leftTime / 1000 - 24 * 60 * 60 * d - 60 * 60 * h - 60 * m
+        ) // 得到最后剩下的毫秒数除以1000 就是秒数，再剩下的毫秒自动忽略即可
+        let hm = Math.floor(
+          (leftTime -
+            24 * 60 * 60 * 1000 * d -
+            60 * 60 * 1000 * h -
+            60 * 1000 * m -
+            s * 1000) /
+            10
+        )
+        if (hm < 10) {
+          hm = '0' + hm
+        }
+        if (s < 10) {
+          s = '0' + s
+        }
+        if (m < 10) {
+          m = '0' + m
+        }
+        if (h < 10) {
+          h = '0' + h
+        }
+        console.log('formatTime----------', d, h, m, s, hm)
+        return `${d ? `${d}天` : ''} ${h}:${m}:${s}:${hm}`
       }
-      if (s < 10) {
-        s = '0' + s
-      }
-      if (m < 10) {
-        m = '0' + m
-      }
-      if (h < 10) {
-        h = '0' + h
-      }
-      return `${d ? `${d}天` : ''} ${h}:${m}:${s}:${hm}`
     }
   }
 }
