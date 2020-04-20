139de87824290e5ec6ee59f5443e6ea7faef2506
2806 <pengling@hydee.cn>
2020-04-20 10:20:48 +0800

    feat:修改已选商品的表头部分

--- src/components/Marketings/SelectedProduct.vue
+++ src/components/Marketings/SelectedProduct.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <el-table ref="dataTable" :data="selectedProducts">
-      <el-table-column property="num" label="门店编码" width="100" />
-      <el-table-column property="name" label="门店名称" width="100" />
-      <el-table-column property="address" label="门店地址" />
+      <el-table-column property="num" label="商品图片" />
+      <el-table-column property="name" label="商品编码" />
+      <el-table-column property="name" label="商品名称" />
+      <el-table-column property="name" label="品牌" />
+      <el-table-column property="name" label="规格" />
+      <el-table-column property="name" label="参考价" />
       <el-table-column label="操作" width="60">
         <template slot-scope="scope">
           <el-button type="text" size="small" @click="handleDel(scope.row)">删除</el-button>
         </template>
       </el-table-column>
     </el-table>
-    <div class="amTips">已选门店{{ selectedProducts.length }}家</div>
+    <div class="amTips">已选商品{{ selectedProducts.length }}个</div>
   </div>
 </template>
 <script>
