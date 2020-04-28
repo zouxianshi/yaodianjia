ac739d56300863a1d1d66434242339fda6465c16
quxianyi <quxianyi@hydee.cn>
2020-04-28 16:33:49 +0800

    feat：领券中心

--- src/views/marketings/activitysManage/coupons/add.vue
+++ src/views/marketings/activitysManage/coupons/add.vue
@@ -347,9 +347,9 @@ export default {
     handletimeRule(timeRule, effectTime) {
       if (timeRule) {
         if (timeRule === 1) {
-          return `自领取${effectTime}天有效`
+          return `自领取起${effectTime}天有效`
         } else if (timeRule === 2) {
-          return `自领取${effectTime.split(',')[0]}天有效,${
+          return `自领取起${effectTime.split(',')[0]}天有效,${
             effectTime.split(',')[1]
           }天失效`
         } else {
