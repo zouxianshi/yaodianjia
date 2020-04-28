066cf50924432eeda998df53cce28268157cf282
2806 <pengling@hydee.cn>
2020-04-28 13:30:33 +0800

    fix:去掉商品图片栏

--- src/views/marketings/_source/SelectedProduct.vue
+++ src/views/marketings/_source/SelectedProduct.vue
@@ -1,45 +1,35 @@
 <template>
   <div class="selected-product-view">
-    <el-table ref="dataTable" :data="selectedProducts.slice((pageInfo.currentPage-1)*pageInfo.pageSize, pageInfo.currentPage*pageInfo.pageSize)" height="250">
-      <el-table-column label="商品图片">
-        <template slot-scope="scope">
-          <el-image
-            style="width: 70px; height: 70px"
-            :src="showImg(scope.row.mainPic)+'?x-oss-process=style/w_80'"
-            lazy
-            fit="contain"
-            :preview-src-list="[`${showImg(scope.row.mainPic)}?x-oss-process=style/w_800`]"
-          />
-        </template>
-      </el-table-column>
+    <el-table
+      ref="dataTable"
+      :data="selectedProducts.slice((pageInfo.currentPage-1)*pageInfo.pageSize, pageInfo.currentPage*pageInfo.pageSize)"
+      height="250"
+    >
       <el-table-column property="erpCode" label="商品编码">
-        <template slot-scope="scope">
-          {{ scope.row.erpCode || scope.row.proCode }}
-        </template>
+        <template slot-scope="scope">{{ scope.row.erpCode || scope.row.proCode }}</template>
       </el-table-column>
       <el-table-column property="name" label="商品名称">
-        <template slot-scope="scope">
-          {{ scope.row.name || scope.row.proName }}
-        </template>
+        <template slot-scope="scope">{{ scope.row.name || scope.row.proName }}</template>
       </el-table-column>
       <el-table-column property="brandName" label="品牌">
-        <template slot-scope="scope">
-          {{ scope.row.brandName || scope.row.proBrand }}
-        </template>
+        <template slot-scope="scope">{{ scope.row.brandName || scope.row.proBrand }}</template>
       </el-table-column>
       <el-table-column property="specSkuList" label="规格">
-        <template slot-scope="scope">
-          {{ scope.row.specSkuList&&scope.row.specSkuList.length > 0 ? scope.row.specSkuList[0].skuValue : scope.row.proSpec }}
-        </template>
+        <template
+          slot-scope="scope"
+        >{{ scope.row.specSkuList&&scope.row.specSkuList.length > 0 ? scope.row.specSkuList[0].skuValue : scope.row.proSpec }}</template>
       </el-table-column>
       <el-table-column property="price" label="参考价">
-        <template slot-scope="scope">
-          {{ scope.row.price || scope.row.proPrice }}
-        </template>
+        <template slot-scope="scope">{{ scope.row.price || scope.row.proPrice }}</template>
       </el-table-column>
       <el-table-column v-if="pageStatus!=3" label="操作" width="60">
         <template slot-scope="scope">
-          <el-button type="text" size="small" :disabled="!!scope.row.proCode" @click="handleDel(scope.row)">删除</el-button>
+          <el-button
+            type="text"
+            size="small"
+            :disabled="!!scope.row.proCode"
+            @click="handleDel(scope.row)"
+          >删除</el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -80,7 +70,9 @@ export default {
         cancelButtonText: '取消',
         type: 'warning'
       }).then(() => {
-        this.selectedProducts = this.selectedProducts.filter(item => item !== row)
+        this.selectedProducts = this.selectedProducts.filter(
+          item => item !== row
+        )
         this.pageInfo.currentPage = 1
         this.$emit('onDel', this.selectedProducts)
       })
@@ -94,7 +86,7 @@ export default {
 </script>
 <style lang="scss">
 .selected-product-view {
-  .goods-logo{
+  .goods-logo {
     width: 55px;
     height: 55px;
   }
