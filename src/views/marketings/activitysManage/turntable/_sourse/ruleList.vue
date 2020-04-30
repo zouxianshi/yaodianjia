45dfceea12c416cd5b4f3adc73a710b4a7e830be
张超 <zhangchao@hydee.cn>
2020-04-30 13:32:59 +0800

    feat:优惠券条形码、大转盘开发

--- src/views/marketings/activitysManage/turntable/_sourse/ruleList.vue
+++ src/views/marketings/activitysManage/turntable/_sourse/ruleList.vue
@@ -7,74 +7,65 @@
       :rules="rules"
       label-width="100px"
       class="demo-ruleForm"
+      size="mini"
     >
-      <el-form-item label="活动名称" prop="name">
-        <el-input v-model="ruleForm.name" style="width:400px" placeholder="最多30字" />
+      <el-form-item label="活动名称" prop="activityDetailName">
+        <el-input v-model="ruleForm.activityDetailName" style="width:400px" placeholder="最多30字" />
       </el-form-item>
-      <el-form-item label="活动时间" prop="value2">
+      <el-form-item label="活动时间">
         <el-date-picker
-          v-model="ruleForm.value2"
+          v-model="activeTime"
           type="datetimerange"
-          :picker-options="pickerOptions"
           range-separator="至"
+          :picker-options="pickerOptions"
           start-placeholder="开始日期"
           end-placeholder="结束日期"
-          align="right"
         />
       </el-form-item>
-      <el-form-item label="活动说明" prop="desc">
-        <el-input v-model="ruleForm.desc" style="width:500px" type="textarea" placeholder="最多200字" />
+      <el-form-item label="活动说明" prop="activityNote">
+        <el-input v-model="ruleForm.activityNote" style="width:500px" type="textarea" maxlength="200" placeholder="最多200字" />
       </el-form-item>
-      <el-form-item label="底部文案" prop="region">
-        <el-input v-model="ruleForm.region" style="width:400px" placeholder="解释说明，最多20字" />
+      <el-form-item label="底部文案" prop="bottomNote">
+        <el-input v-model="ruleForm.bottomNote" style="width:400px" placeholder="解释说明，最多20字" />
       </el-form-item>
-      <div v-if="intrShow" style="font-size:13px;margin-left:20px">
-        大转盘的玩法场景说明：
-        <ul>
-          <li>1. 免费参与：创建抽奖活动后，将链接发布至商城首页或打印活动二维码，会员进入免费参与抽奖，较适用于小面额优惠券抽奖，提高用户活跃，促进商品销售；</li>
-          <li>2. 积分参与：会员需要消耗一定的积分才能参与抽奖， 消耗会员积分库存，设置活动后可以将链接发布至微商城广告位，或打印二维码制作海报，会员扫码参与抽奖；</li>
-          <li>3. 活动参与：适用于支付满一定金额获得抽奖机会的奖励，可以提高用户活跃，创建一个大转盘活动后，设置抽奖次数，然后在支付有礼中，添加支付后权益为参与抽奖，会员在微商城或线下消费满足条件后，触发抽奖机会入口，会员进入参与抽奖。</li>
-        </ul>
-      </div>
-      <div class="rule-ruleList-title">
-        活动规则
-        <i class="el-icon-warning-outline" @click="()=>intrShow?intrShow=false:intrShow=true" />
-      </div>
-      <el-form-item label="参与方式" prop="participatio">
-        <div class="rule-ruleList-participatio">
-          <div>
-            <el-radio v-model="ruleForm.participatio" label="1">免费参与</el-radio>
-          </div>
-          <div>
-            <el-radio v-model="ruleForm.participatio" label="2">
-              消耗积分&emsp;每消耗&emsp;
-              <el-input v-model="ruleForm.participatioInput" style="width:100px" />&emsp;积分，参与一次
-            </el-radio>
-          </div>
-          <div>
-            <el-radio v-model="ruleForm.participatio" label="3">免费参与</el-radio>
-          </div>
+      <el-form-item>
+        <div v-if="intrShow" style="font-size:13px;margin-left:20px">
+          大转盘的玩法场景说明：
+          <ul>
+            <li>1. 免费参与：创建抽奖活动后，将链接发布至商城首页或打印活动二维码，会员进入免费参与抽奖，较适用于小面额优惠券抽奖，提高用户活跃，促进商品销售；</li>
+            <li>2. 积分参与：会员需要消耗一定的积分才能参与抽奖， 消耗会员积分库存，设置活动后可以将链接发布至微商城广告位，或打印二维码制作海报，会员扫码参与抽奖；</li>
+            <li>3. 活动参与：适用于支付满一定金额获得抽奖机会的奖励，可以提高用户活跃，创建一个大转盘活动后，设置抽奖次数，然后在支付有礼中，添加支付后权益为参与抽奖，会员在微商城或线下消费满足条件后，触发抽奖机会入口，会员进入参与抽奖。</li>
+          </ul>
+        </div>
+        <div slot="label">
+          活动规则
+          <i class="el-icon-warning-outline" @click="()=>intrShow?intrShow=false:intrShow=true" />
         </div>
       </el-form-item>
-      <el-form-item label="抽奖次数" prop="sweepstakes">
-        <div class="rule-ruleList-participatio">
-          <div>
-            <el-radio v-model="ruleForm.sweepstakes" label="1">
-              <span>
-                每天可抽奖&emsp;
-                <el-input v-model="ruleForm.everyday" style="width:100px" />
-              </span>
-            </el-radio>
-          </div>
-          <div>
-            <el-radio v-model="ruleForm.sweepstakes" label="2">
-              <span>
-                每人可抽奖&emsp;
-                <el-input v-model="ruleForm.people" style="width:100px" />
-              </span>
-            </el-radio>
-          </div>
-        </div>
+      <el-form-item label="参与方式" prop="integralRule">
+        <el-radio-group v-model="ruleForm.joinRule" style="width:120px;font-size: 18px;line-height: inherit;">
+          <el-radio :label="1">免费参与</el-radio>
+          <el-radio :label="2">消耗积分&emsp;每消耗&emsp;
+            <el-input v-model="ruleForm.integralRule" :disabled="ruleForm.joinRule !== 2" maxlength="8" style="width:100px" />&emsp;积分，参与一次
+          </el-radio>
+          <el-radio :label="3">免费参与</el-radio>
+        </el-radio-group>
+      </el-form-item>
+      <el-form-item label="抽奖次数">
+        <el-radio-group v-model="ruleForm.countType" style="width:120px;font-size: 18px;line-height: inherit;">
+          <el-radio :label="2">
+            <span>
+              每天可抽奖&emsp;
+              <el-input v-model="dayLimit" :disabled="ruleForm.countType===1" maxlength="6" style="width:100px" />
+            </span>
+          </el-radio>
+          <el-radio :label="1">
+            <span>
+              每人可抽奖&emsp;
+              <el-input v-model="personLimit" :disabled="ruleForm.countType===2" maxlength="6" style="width:100px" />
+            </span>
+          </el-radio>
+        </el-radio-group>
       </el-form-item>
     </el-form>
     <div style="margin-left:30px">
@@ -83,32 +74,13 @@
   </div>
 </template>
 <script>
+import { formatDate } from '@/utils/timer'
 export default {
   name: 'GiftCard',
-  // props: {
-
-  // },
   data() {
-    var validatesweepstake = (rule, value, callback) => {
-      if (value === '1') {
-        if (this.ruleForm.everyday === '') {
-          callback(new Error('请输入每日抽奖次数'))
-        } else {
-          callback()
-        }
-      } else {
-        if (this.people === '') {
-          callback(new Error('请输入每人抽奖次数'))
-        } else {
-          callback()
-        }
-      }
-    }
     var validateparticipatio = (rule, value, callback) => {
-      if (value === '2') {
-        if (this.ruleForm.participatioInput === '') {
-          callback(new Error('请输入消耗积分数'))
-        }
+      if (this.ruleForm.joinRule === 2 && (value === 0 || ('' + value).trim() === '')) {
+        callback(new Error('请输入大于0的消耗积分数'))
       } else {
         callback()
       }
@@ -116,75 +88,61 @@ export default {
     return {
       intrShow: false,
       pickerOptions: {
-        shortcuts: [
-          {
-            text: '最近一周',
-            onClick(picker) {
-              const end = new Date()
-              const start = new Date()
-              start.setTime(start.getTime() - 3600 * 1000 * 24 * 7)
-              picker.$emit('pick', [start, end])
-            }
-          },
-          {
-            text: '最近一个月',
-            onClick(picker) {
-              const end = new Date()
-              const start = new Date()
-              start.setTime(start.getTime() - 3600 * 1000 * 24 * 30)
-              picker.$emit('pick', [start, end])
-            }
-          },
-          {
-            text: '最近三个月',
-            onClick(picker) {
-              const end = new Date()
-              const start = new Date()
-              start.setTime(start.getTime() - 3600 * 1000 * 24 * 90)
-              picker.$emit('pick', [start, end])
-            }
-          }
-        ]
+        disabledDate(time) {
+          return time.getTime() < new Date(new Date().getTime() - 86400000)
+        }
       },
+      activeTime: [], // 活动有效期
+      dayLimit: 0, // 每天限制
+      personLimit: 0, // 每人限制
       ruleForm: {
-        name: '',
-        region: '',
-        desc: '',
-        value2: '',
-        participatio: '1',
-        sweepstakes: '1',
+        activityDetailName: '', // 活动名称
+        bottomNote: '', // 底部文案
+        beginTime: '', // 活动开始时间
+        endTime: '', // 活动结束时间
+        activityNote: '', // 活动说明
+        integralRule: '1', // 参与消耗积分
+        joinRule: 1, // 参与方式
+        countType: 2, // 参与限制类型  1：每人 2：每天
         participatioInput: '',
-        everyday: '',
-        people: ''
+        countRule: 0 // 次数限制
       },
       rules: {
-        name: [
+        activityDetailName: [
           { required: true, message: '请输入活动名称', trigger: 'blur' },
           { min: 1, max: 30, message: '最多30字', trigger: 'blur' }
         ],
-        region: [
+        bottomNote: [
           { required: true, message: '请输入底部文案', trigger: 'blur' },
           { min: 1, max: 20, message: '最多20字', trigger: 'blur' }
         ],
-        value2: [
-          {
-            required: true,
-            message: '请选择日期',
-            trigger: 'change'
-          }
-        ],
-        desc: [
+        activityNote: [
           { required: true, message: '请填写活动说明', trigger: 'blur' },
           { min: 1, max: 200, message: '最多200字', trigger: 'blur' }
         ],
-        sweepstakes: [{ validator: validatesweepstake, trigger: 'change' }],
-        participatio: [{ validator: validateparticipatio, trigger: 'change' }]
+        integralRule: [{ validator: validateparticipatio, trigger: 'blur' }] // 参与消耗积分
       }
     }
   },
-  watch: {},
+  watch: {
+    activeTime(newVal) {
+      this.ruleForm.beginTime = formatDate(newVal[0])
+      this.ruleForm.endTime = formatDate(newVal[1])
+    },
+    personLimit(newVal) { // 监听按人限制变化
+      if (this.ruleForm.countType === 1) {
+        this.ruleForm.countRule = newVal
+      }
+    },
+    dayLimit(newVal) {
+      if (this.ruleForm.countType === 2) {
+        this.ruleForm.countRule = newVal
+      }
+    }
+  },
   methods: {
     submitForm(formName) {
+      console.log(this.ruleForm)
       this.$refs[formName].validate(valid => {
         if (valid) {
           alert('submit!')
@@ -215,12 +173,6 @@ export default {
   }
   .el-icon-warning-outline:hover {
     cursor: pointer;
-  }
-  .rule-ruleList-participatio {
-    margin-top: 30px;
-    div {
-      margin: 5px 0;
-    }
   }
 }
 </style>
