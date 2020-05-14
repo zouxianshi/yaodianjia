dfe7272bc57fdf4e6850c70011ecd67ca0ffd7f5
quxianyi <quxianyi@hydee.cn>
2020-05-14 18:43:49 +0800

    feat：海贝兑换接口

---
+++ src/api/equity.js
@@ -0,0 +1,11 @@
+import request from '@/utils/request'
+import config from '@/utils/config'
+// import qs from 'qs'
+// 新增或修改商户配置信息
+export function editMerChantSet(params) {
+  return request({
+    url: `${config.merGoods}/1.0/merSet/editMerChantSet`,
+    method: 'post',
+    data: params
+  })
+}
