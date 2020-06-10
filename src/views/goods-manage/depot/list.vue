9375ccc9d24c534665a1f55d6f1ca47b2986fa1b
zwj <zwj865738120@163.com>
2020-06-10 18:58:50 +0800

    feat：【商品库】删除操作展示逻辑变更

--- src/views/goods-manage/depot/list.vue
+++ src/views/goods-manage/depot/list.vue
@@ -266,7 +266,7 @@
                     <el-button type="text" size="mini">编辑</el-button>
                   </a>
                 </template>
-                <template v-if="!scope.row.specId">
+                <template>
                   <el-divider direction="vertical" />
                   <el-button type="text" size="mini" @click="handleDel(scope.row)">删除</el-button>
                 </template>
