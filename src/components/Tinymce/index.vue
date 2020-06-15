21ec6f2234a00bd3ebe681e5c3c9be5228e82128
张超 <zhangchao@hydee.cn>
2020-06-15 18:28:13 +0800

    fixbug: 解决直播插件合并冲突问题

---
+++ src/components/Tinymce/index.vue
@@ -0,0 +1,277 @@
+<template>
+  <div :class="{fullscreen:fullscreen}" class="tinymce-container" :style="{width:containerWidth}">
+    <textarea
+      :id="tinymceId"
+      class="tinymce-textarea"
+      :placeholder="placeholder"
+    />
+    <div class="editor-custom-btn-container">
+      <!-- <editorImage color="#1890ff" class="editor-upload-btn" @successCBK="imageSuccessCBK" /> -->
+    </div>
+  </div>
+</template>
+
+<script>
+/**
+ * docs:
+ * https://panjiachen.github.io/vue-element-admin-site/feature/component/rich-editor.html#tinymce
+ */
+// import editorImage from './components/EditorImage'
+import plugins from './plugins'
+import toolbar from './toolbar'
+// import load from './dynamicLoadScript'
+import config from '@/utils/config'
+import '../../assets/lang/zh_CN'
+import { mapGetters } from 'vuex'
+export default {
+  name: 'Tinymce',
+  // components: { editorImage },
+  props: {
+    id: {
+      type: String,
+      default: function() {
+        return (
+          'vue-tinymce-' +
+          +new Date() +
+          ((Math.random() * 1000).toFixed(0) + '')
+        )
+      }
+    },
+    value: {
+      type: String,
+      default: ''
+    },
+    placeholder: {
+      type: String,
+      default: ''
+    },
+    toolbar: {
+      type: Array,
+      required: false,
+      default() {
+        return []
+      }
+    },
+    readonly: {
+      // 只读
+      type: Boolean,
+      default: false
+    },
+    menubar: {
+      type: String,
+      default: 'file edit insert view format table'
+    },
+    height: {
+      type: [Number, String],
+      required: false,
+      default: 360
+    },
+    width: {
+      type: [Number, String],
+      required: false,
+      default: 'auto'
+    }
+  },
+  data() {
+    return {
+      hasChange: false,
+      hasInit: false,
+      tinymceId: this.id,
+      fullscreen: false,
+      languageTypeList: {
+        en: 'en',
+        zh: 'zh_CN',
+        es: 'es_MX',
+        ja: 'ja'
+      }
+    }
+  },
+  computed: {
+    containerWidth() {
+      const width = this.width
+      if (/^[\d]+(\.[\d]+)?$/.test(width)) {
+        // matches `100`, `'100'`
+        return `${width}px`
+      }
+      return width
+    },
+    upLoadUrl() {
+      return `${this.uploadFileURL}/${config.merGoods}/1.0/file/_upload`
+    },
+    ...mapGetters(['token', 'merCode'])
+  },
+  watch: {
+    value(val) {
+      if (!this.hasChange && this.hasInit) {
+        this.$nextTick(() =>
+          window.tinymce.get(this.tinymceId).setContent(val || '')
+        )
+      }
+    }
+  },
+  mounted() {
+    this.init()
+  },
+  activated() {
+    if (window.tinymce) {
+      this.initTinymce()
+    }
+  },
+  deactivated() {
+    this.destroyTinymce()
+  },
+  destroyed() {
+    this.destroyTinymce()
+  },
+  methods: {
+    init() {
+      // dynamic load tinymce from cdn
+      // load(tinymceCDN, (err) => {
+      //   if (err) {
+      //     this.$message.error(err.message)
+      //     return
+      //   }
+
+      // })
+      this.initTinymce()
+    },
+    initTinymce() {
+      const _this = this
+      window.tinymce.init({
+        selector: `#${this.tinymceId}`,
+        language: this.languageTypeList['zh'],
+        height: this.height,
+        min_height: 350,
+        max_height: 650,
+        body_class: 'panel-body ',
+        object_resizing: false,
+        toolbar: this.toolbar.length > 0 ? this.toolbar : toolbar,
+        menubar: this.menubar,
+        removed_menuitems: 'spellchecker,paste,copy,cut,pastetext',
+        plugins: plugins,
+        readonly: this.readonly,
+        end_container_on_empty_block: true,
+        powerpaste_word_import: 'clean',
+        code_dialog_height: 450,
+        code_dialog_width: 1000,
+        advlist_bullet_styles: 'square',
+        advlist_number_styles: 'default',
+        imagetools_cors_hosts: ['www.tinymce.com', 'codepen.io'],
+        default_link_target: '_blank',
+        link_title: false,
+        nonbreaking_force_tab: true, // inserting nonbreaking space &nbsp; need Nonbreaking Space Plugin
+        init_instance_callback: editor => {
+          _this.hasInit = true
+          if (this.id === 'basicInfo') {
+            this.$emit('onload')
+          }
+          if (_this.value) {
+            editor.setContent(_this.value)
+          }
+          editor.on('NodeChange Change KeyUp SetContent', () => {
+            this.hasChange = true
+            this.$emit('input', editor.getContent())
+          })
+        },
+        setup(editor) {
+          editor.on('FullscreenStateChanged', e => {
+            _this.fullscreen = e.state
+          })
+        },
+        images_upload_handler: (blobInfo, success, failure) => {
+          const file = blobInfo.blob()
+          const isImg =
+            file.type === 'image/jpeg' ||
+            file.type === 'image/png' ||
+            file.type === 'image/jpg'
+          const isLt2M = file.size / 1024 / 1024 < 2
+          if (!isImg) {
+            failure('只能上传格式为 jpg、jpeg、png的图片')
+            return false
+          }
+          if (!isLt2M) {
+            failure('上传图片大小不能超过 2MB!')
+            return false
+          }
+          const xhr = new XMLHttpRequest()
+          xhr.withCredentials = false
+          xhr.open('POST', `${this.upLoadUrl}?merCode=${this.merCode}`)
+          xhr.setRequestHeader('Authorization', this.$store.getters.token)
+          const formData = new FormData()
+          formData.append('file', file)
+          var _this = this
+          xhr.onload = function(e) {
+            var json
+            if (xhr.status !== 200) {
+              failure('HTTP Error: ' + xhr.status)
+              return
+            }
+            json = JSON.parse(this.responseText)
+
+            if (!json || typeof json.data !== 'string') {
+              failure('Invalid JSON: ' + xhr.responseText)
+              return
+            }
+            // console.log(json.location)
+            success(_this.showImg(json.data))
+          }
+          xhr.send(formData)
+        }
+      })
+    },
+    destroyTinymce() {
+      const tinymce = window.tinymce.get(this.tinymceId)
+      if (this.fullscreen) {
+        tinymce.execCommand('mceFullScreen')
+      }
+
+      if (tinymce) {
+        tinymce.destroy()
+      }
+    },
+    setContent(value) {
+      window.tinymce.get(this.tinymceId).setContent(value)
+    },
+    getContent() {
+      window.tinymce.get(this.tinymceId).getContent()
+    },
+    imageSuccessCBK(arr) {
+      const _this = this
+      arr.forEach(v => {
+        window.tinymce
+          .get(_this.tinymceId)
+          .insertContent(
+            `<video control class="wscnph" style="width:100%" src="${v.url}" >`
+          )
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+.tinymce-container {
+  position: relative;
+  line-height: normal;
+}
+.tinymce-container >>> .mce-fullscreen {
+  z-index: 10000;
+}
+.tinymce-textarea {
+  visibility: hidden;
+  z-index: -1;
+}
+.editor-custom-btn-container {
+  position: absolute;
+  right: 4px;
+  top: 4px;
+  /*z-index: 2005;*/
+}
+.fullscreen .editor-custom-btn-container {
+  z-index: 10000;
+  position: fixed;
+}
+.editor-upload-btn {
+  display: inline-block;
+}
+</style>
