8ae79c0ec168d008f64d40d93cc30b730b04a3d4
zwj <zwj865738120@163.com>
2020-06-22 11:49:45 +0800

    fix：商品库列表页跳路由跳出时去掉弹框提示、修复商品信息编辑的图文详情没有回写的bug

--- src/views/goods-manage/new-goods/edit.vue
+++ src/views/goods-manage/new-goods/edit.vue
@@ -1228,6 +1228,7 @@ export default {
     // 加载商品详情
     _loadGoodsDetails(val) {
       this.goodsIntro.content = val
+      this.$refs['details-ty'].setContent(val)
       // this.$refs['details-ty'].destroyTinymce() // 先销毁
       // getGoodsDetails(id)
       //   .then(res => {
