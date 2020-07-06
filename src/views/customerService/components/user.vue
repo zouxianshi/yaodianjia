25c1eaa82b9f73cbc4912cc0a78cead2e36d8457
zhangxinbo <zhangxinbo@hydee.cn>
2020-07-06 17:50:48 +0800

    feat: 客服B端消息排序

--- src/views/customerService/components/user.vue
+++ src/views/customerService/components/user.vue
@@ -94,7 +94,7 @@ export default {
     }
   },
   created() {
-    console.log('this', this, this.getEmojiList)
+    console.log('this-cusyomerService', this, this.getEmojiList)
   },
   methods: {
     handleClick() {
