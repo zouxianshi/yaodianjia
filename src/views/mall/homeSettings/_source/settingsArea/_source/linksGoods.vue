9e8aef4b884d347e256e27dbf32c16bc69aab4b4
quanquan <quanquansy@gmail.com>
2020-07-16 09:36:40 +0800

    fix:修复选择链接商品详情增加名称 编码等字段

--- src/views/mall/homeSettings/_source/settingsArea/_source/linksGoods.vue
+++ src/views/mall/homeSettings/_source/settingsArea/_source/linksGoods.vue
@@ -5,13 +5,26 @@
     </div>
     <div v-loading="loading" class="lgm-list-box">
       <el-table :data="list" style="width: 100%" size="mini" class="scrollbar" height="calc(100vh - 304px)" @row-click="onRowClick">
-        <el-table-column label="" width="50">
+        <el-table-column label="" width="30">
           <template slot-scope="scope">
             <el-radio v-model="linkUrl" :label="scope.row.linkAddress">&nbsp;</el-radio>
           </template>
         </el-table-column>
-        <el-table-column prop="name" label="商品名称" show-overflow-tooltip width="100" />
-        <el-table-column prop="linkAddress" label="链接" show-overflow-tooltip />
+        <el-table-column prop="name" label="商品图片" width="70">
+          <template slot-scope="scope">
+            <el-image style="width: 36px; height: 36px" :src="showImg(scope.row.mainPic)" :preview-src-list="[showImg(scope.row.mainPic)]" fit="fill" />
+          </template>
+        </el-table-column>
+        <el-table-column prop="name" label="商品名称" show-overflow-tooltip />
+        <el-table-column prop="erpCode" label="商品编码" show-overflow-tooltip width="70" />
+        <el-table-column prop="brandName" label="品牌" show-overflow-tooltip width="70" />
+        <el-table-column prop="specStr" label="规格" show-overflow-tooltip />
+        <el-table-column label="参考价" show-overflow-tooltip width="70">
+          <template slot-scope="scope">
+            <span style="color: #ff0000;">¥{{ scope.row.mprice }}</span>
+          </template>
+        </el-table-column>
+        <!--<el-table-column prop="linkAddress" label="链接" show-overflow-tooltip />-->
       </el-table>
       <div class="lgm-pages">
         <el-pagination :current-page="searchParams.currentPage" :page-size="searchParams.pageSize" :total="totalCount" @current-change="onPage" />
